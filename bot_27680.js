8b8c821013c324587807ddb0c5fd13380932dbb3
AngelCityBusiness <158473688+AngelCityBusiness@users.noreply.github.com>
2024-07-21 23:40:46 -0700

    Update bot_27680.js

--- bot_27680.js
+++ bot_27680.js
@@ -4,14 +4,12 @@
 // I recommend a file name like "#####_business_name.js"
 
 (function() {
-
-    
     // Load the chat plugin
     var script = document.createElement('script');
     
     script.src = 'https://app.aminos.ai/js/chat_plugin.js'; // Regular website Original Script. // Comment out if not using.
-    //script.src = 'https://app.aminos.ai/js/chat_plugin_ghl.js'; // GoHighLevel Original Script. // Comment out if not using.
-    //script.src = 'https://app.aminos.ai/js/chat_form_plugin.js'; // Inline Website Original Script. // Comment out if not using.
+    // script.src = 'https://app.aminos.ai/js/chat_plugin_ghl.js'; // GoHighLevel Original Script. // Comment out if not using.
+    // script.src = 'https://app.aminos.ai/js/chat_form_plugin.js'; // Inline Website Original Script. // Comment out if not using.
     
     script.setAttribute('data-bot-id', '27680'); // Input Bot ID here by replacing ##### with your bot number.
     document.head.appendChild(script);
@@ -30,10 +28,10 @@
     document.head.appendChild(style);
     
     //************************************************
-    //NEW INTEGRATION CODE: <script src="https://chatbot.angelpro.ai/bot_27680.js"></script>
-     //MAKE SURE TO ADD: <div id="chat_form"></div>
-     //WITHIN THE HTML BLOCK IF THE FILE IS FOR IFRAME EMBED
+    // NEW INTEGRATION CODE: <script src="https://chatbot.angelpro.ai/bot_27680.js"></script>
+    // MAKE SURE TO ADD: <div id="chat_form"></div>
+    // WITHIN THE HTML BLOCK IF THE FILE IS FOR IFRAME EMBED
 
-    //Notice the Integration code does NOT include the repository name. The repo name is not necessary for the integration code.
+    // Notice the Integration code does NOT include the repository name. The repo name is not necessary for the integration code.
     //************************************************
 })();
